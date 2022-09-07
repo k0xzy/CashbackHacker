dcfeecb175d540bb13cfb37ae7d0070f3a2c661e
k0xzy <koxzy22@gmail.com>
2022-09-07 11:48:27 +0300

    TestNG

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerTest {
+
+    @Test
+    public void NormalRestL() {
+        CashBackHacker service = new CashBackHacker();
+        int expected = 100;
+        int amount = 900;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void NormalRestH() {
+        CashBackHacker service = new CashBackHacker();
+        int expected = 900;
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ZeroRest() {
+        CashBackHacker service = new CashBackHacker();
+        int expected = 0;
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
